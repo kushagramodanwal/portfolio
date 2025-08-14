e1f9d6eb8a3e816ea45e0db714612678b9100c38
kushagra <kushagramodanwal@gmail.com>
2025-08-14 23:05:46 +0530

    J

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Blogs = () => {
   return (
-    <div className="p-10 bg-white dark:bg-black min-h-screen">
+    <div className="pt-16 p-10 bg-white dark:bg-black min-h-screen">
       <h1 className="text-3xl font-bold text-black dark:text-white">Blogs Page</h1>
       <p className="text-black dark:text-white mt-4">
         Here you can read my latest blog posts!
