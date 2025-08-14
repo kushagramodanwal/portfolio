3fb90df125c02087083ae28a56e1aacba5641e93
kushagra <kushagramodanwal@gmail.com>
2025-08-14 19:31:30 +0530

    doit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
 
 const Navbar = ({ darkMode, setDarkMode }) => {
   return (
-    <nav className="flex justify-between items-center py-3 font-bold left-0 right-0 top-0 fixed z-20 px-8 white dark:bg-gray-800 text-black dark:text-white">
+    <nav className="flex justify-between items-center py-3 font-bold left-0 right-0 top-0 fixed z-20 px-8 white dark:black text-black dark:text-white">
       <ul className="flex gap-8 items-center text-sm">
         <li>
           <Link
